921f614273b5c37c6d10e5d55b1789fdd63430b2
killReal99 <85346432+killReal99@users.noreply.github.com>
2023-11-02 14:37:42 +0300

    Add xml version

--- cppcheck.cmake
+++ cppcheck.cmake
@@ -9,6 +9,8 @@ add_custom_target(
         COMMAND /usr/bin/cppcheck
         --enable=all
         --force
+        --xml
+        --xml-version=2
         --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
         ${ALL_SOURCE_FILES}
         2> cppcheck-result.xml
